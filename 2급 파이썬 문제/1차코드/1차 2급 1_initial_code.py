a05eaa3ddcc783f20bb8aa9d0818e8ed579c9091
seon <darkness.blooms.sun@gmail.com>
2023-07-11 12:50:20 +0900

    1번문제풀이

--- 2급 파이썬 문제/1차코드/1차 2급 1_initial_code.py
+++ 2급 파이썬 문제/1차코드/1차 2급 1_initial_code.py
@@ -2,8 +2,16 @@
 #import math
 
 def solution(shirt_size):
-    #Write code here.
+    dit={'XS':0,'S':0,'M':0,'L':0,'XL':0,'XXL':0}
+    for i in shirt_size:
+        if i in dit:
+            dit[i]+=1
+        else:
+            dit[i]=1
     answer = []
+    for j in dit:
+        answer.append(dit[j])
+
     return answer
 
 #The following is code to output testcase.
