68ecabc81edcf2407972b0a17167c73f8432fa46
seon <darkness.blooms.sun@gmail.com>
2023-07-11 16:26:23 +0900

    8번 풀이

--- 2급 파이썬 문제/1차코드/1차 2급 8_initial_code.py
+++ 2급 파이썬 문제/1차코드/1차 2급 8_initial_code.py
@@ -1,10 +1,12 @@
 def solution(sentence):
     str = ''
     for c in sentence:
-        if c != '.' or c != ' ':
+        if c != '.' and c != ' ':
             str += c
+    print(str)
     size = len(str)
     for i in range(size // 2):
+        print(str[i],str[size-1-i])
         if str[i] != str[size - 1 - i]:
             return False
     return True
